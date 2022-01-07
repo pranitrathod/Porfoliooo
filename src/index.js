5d5565b81e9f2acab0cb9e7c263ba9093bcbb009
Prant <pranitrathod007@gmail.com>
2022-01-07 12:47:27 +0530

    Added visitors counter

--- src/index.js
+++ src/index.js
@@ -4,3 +4,19 @@ import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+
+var Change_Visitors_text=document.querySelector(".visitors");
+var visitCounts=localStorage.getItem("page_view");
+
+if (visitCounts)
+{
+    visitCount=Number(visitCounts)+1;
+    localStorage.getItem("page_view",visitCounts);
+
+}
+else{
+    visitCounts=1;
+    localStorage.getItem("page_view",visitCounts);
+}
+
+Change_Visitors_text.innerHTML="Visitors:",visitCounts;
